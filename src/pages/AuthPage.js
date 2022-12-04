59e19b2d3665dfac61edc30f0c49b4c0635a36d6
denis <denis.usikov95@mail.ru>
2022-12-04 10:21:46 +0300

    frontend+backend proxy

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,58 +1,80 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useHttp} from "../hooks/http.hook";
 
+
 export const AuthPage = () => {
+	const {loading, request, error, clearError} = useHttp()
+	const [form, setForm] = useState({
+		email: "", password: ""
+	})
 
-    const [form, setForm] = useState({
-        email: "", password: ""
-    })
+	useEffect(()=>{
 
-    const changeHandler = event => {
-        setForm({...form, [event.target.name]: event.target.value})
-    }
+	}, [error])
 
+	const changeHandler = event => {
+		setForm({...form, [event.target.name]: event.target.value})
+	}
 
+	const registerHandler = async () => {
+		try {
+			const data = await request("/api/auth/register", "POST", {...form})
+			console.log("data", data)
+		} catch (e) {
 
-    return (
-        <div className="row">
-            <h1 className="center-align">Авторизация</h1>
-            <div className="col s6 offset-s3">
-                <div className="card blue darken-1">
-                    <div className="card-content white-text">
-                        <span className="card-title center-align">Авторизация</span>
-                        <div>
-                            <div className="input-field">
-                                <input
-                                    className="yellow-input"
-                                    placeholder="Введите email"
-                                    id="email"
-                                    type="text"
-                                    name="email"
-                                    onChange={changeHandler}
-                                />
-                                <label htmlFor="email">Email</label>
-                            </div>
+		}
+	}
 
-                            <div className="input-field">
-                                <input
-                                    className="yellow-input"
-                                    placeholder="Введите пароль"
-                                    id="password"
-                                    type="password"
-                                    name="password"
-                                    onChange={changeHandler}
-                                />
-                                <label htmlFor="email">Пароль</label>
-                            </div>
+	return (<div className="row">
+		<h1 className="center-align">Авторизация</h1>
+		<div className="col s6 offset-s3">
+			<div className="card blue darken-1">
+				<div className="card-content white-text">
+					<span className="card-title center-align">Авторизация</span>
+					<div>
+						<div className="input-field">
+							<input
+								className="yellow-input"
+								placeholder="Введите email"
+								id="email"
+								type="text"
+								name="email"
+								onChange={changeHandler}
+							/>
+							<label htmlFor="email">Email</label>
+						</div>
 
-                        </div>
-                    </div>
-                    <div className="card-action">
-                        <button className="btn yellow darken-4" style={{marginRight: 20}}>Войти</button>
-                        <button className="btn grey lighten-1 black-text">Регистрация</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+						<div className="input-field">
+							<input
+								className="yellow-input"
+								placeholder="Введите пароль"
+								id="password"
+								type="password"
+								name="password"
+								onChange={changeHandler}
+							/>
+							<label htmlFor="email">Пароль</label>
+						</div>
+
+					</div>
+				</div>
+				<div className="card-action">
+					<button
+						className="btn yellow darken-4"
+						style={{marginRight: 20}}
+						disabled={loading}
+					>
+						Войти
+					</button>
+					<button
+						className="btn grey lighten-1 black-text"
+						onClick={registerHandler}
+						disabled={loading}
+					>
+						Регистрация
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>)
 }
